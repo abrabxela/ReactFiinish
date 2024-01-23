54d0367dd6c68fd4bb56d6cdd1711c0687070f18
Oleksii Barba <abrabxela@gmail.com>
2024-01-23 23:18:10 +0200

    add page films

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,7 +1,55 @@
+import { Box, Grid } from "@mui/material";
+import useRequest from "../hooks/UseRequest";
+import SingleCard from "../components/SingleCard/SingleCard";
+import { DEFAULT_IMAGE_FILM } from "../constants/imgFilm";
+
+
+
+
 function Films() {
-    return(
-        <h1>This is films page</h1>
-    )
+    const apiData = useRequest("");
+  return (
+    <>
+      <Box
+        style={{
+          minHeight: "400px",
+          backgroundImage: `url(https://gorodok.bz/upload/medialibrary/422/42202c1cb89675e8215c7c347d8d96d9.jpg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          color: "white",
+          padding: "2px",
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{ padding: 10, justifyContent: "center", alignItems: "center" }}
+        >
+          {apiData.map(({ show }, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={index}
+              sx={{ height: "250px" }}
+            >
+              <SingleCard
+                id={show.id}
+                name={show.name}
+                time={show.premiered}
+                image={show.image ? show.image.medium : DEFAULT_IMAGE_FILM}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
 export default Films;
